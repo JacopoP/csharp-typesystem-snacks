ef74b7e0a6d006670c27cd11952904f52ed3743b
Jac22 <jacopo.prudentino@gmail.com>
2023-04-05 14:38:38 +0200

    Completed snack 2

--- Program.cs
+++ Program.cs
@@ -1,18 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+
+
 //L’utente inserisce due numeri in successione. Il software stampa il maggiore.
 
-Console.WriteLine("Inserisci due numeri: ");
-double num1, num2;
+//Console.WriteLine("Inserisci due numeri: ");
+//double num1, num2;
 
-double.TryParse(Console.ReadLine(), out num1);
+//double.TryParse(Console.ReadLine(), out num1);
 
-double.TryParse(Console.ReadLine(), out num2);
+//double.TryParse(Console.ReadLine(), out num2);
 
-if (num1 > num2)
-    Console.WriteLine($"Il numero maggiore è {num1}");
-else if (num1 < num2)
-    Console.WriteLine($"Il numero maggiore è {num2}");
+//if (num1 > num2)
+//    Console.WriteLine($"Il numero maggiore è {num1}");
+//else if (num1 < num2)
+//    Console.WriteLine($"Il numero maggiore è {num2}");
+//else
+//    Console.WriteLine("Hai inserito 2 numeri uguali");
+
+
+
+//L’utente inserisce due parole in successione. Il software stampa prima la parola più corta, poi la parola più lunga.
+
+Console.WriteLine("Scrivi due parole: ");
+
+string? str1 = Console.ReadLine(), str2 = Console.ReadLine();
+
+if (str1.Length > str2.Length)
+    Console.WriteLine($"{str2} {str1}");
+else if (str2.Length > str1.Length)
+    Console.WriteLine($"{str1} {str2}");
 else
-    Console.WriteLine("Hai inserito 2 numeri uguali");
+    Console.WriteLine("Hai inserito 2 parole di egual lunghezza");
