b0c85a6a1de7e78a3be671cc445861ce0a4ff083
Jac22 <jacopo.prudentino@gmail.com>
2023-04-05 14:31:18 +0200

    Completed snack 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//L’utente inserisce due numeri in successione. Il software stampa il maggiore.
+
+Console.WriteLine("Inserisci due numeri: ");
+double num1, num2;
+
+double.TryParse(Console.ReadLine(), out num1);
+
+double.TryParse(Console.ReadLine(), out num2);
+
+if (num1 > num2)
+    Console.WriteLine($"Il numero maggiore è {num1}");
+else if (num1 < num2)
+    Console.WriteLine($"Il numero maggiore è {num2}");
+else
+    Console.WriteLine("Hai inserito 2 numeri uguali");
